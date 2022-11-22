53b7d48c7241bb445f836b35a965182867ef5c08
raheemadamboev <vividricemaryandmax@gmail.com>
2022-11-22 14:22:17 +0500

    setup material3

--- app/src/main/java/xyz/teamgravity/composedeeplink/Main.kt
+++ app/src/main/java/xyz/teamgravity/composedeeplink/Main.kt
@@ -6,13 +6,10 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import xyz.teamgravity.composedeeplink.ui.theme.ComposeDeepLinkTheme
 
-class MainActivity : ComponentActivity() {
+class Main : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {

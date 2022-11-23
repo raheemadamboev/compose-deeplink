7e0ee34600e59377090f984d10cd7b2c14cfe098
raheemadamboev <vividricemaryandmax@gmail.com>
2022-11-23 12:29:06 +0500

    setup nav host and deep links

--- app/src/main/java/xyz/teamgravity/composedeeplink/Main.kt
+++ app/src/main/java/xyz/teamgravity/composedeeplink/Main.kt
@@ -1,12 +1,24 @@
 package xyz.teamgravity.composedeeplink
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import androidx.navigation.navDeepLink
 import xyz.teamgravity.composedeeplink.ui.theme.ComposeDeepLinkTheme
 
 class Main : ComponentActivity() {
@@ -18,7 +30,50 @@ class Main : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    val controller = rememberNavController()
 
+                    NavHost(
+                        navController = controller,
+                        startDestination = "home"
+                    ) {
+                        composable(route = "home") {
+                            Box(
+                                contentAlignment = Alignment.Center,
+                                modifier = Modifier.fillMaxSize()
+                            ) {
+                                Button(
+                                    onClick = {
+                                        controller.navigate("detail")
+                                    }
+                                ) {
+                                    Text(text = stringResource(id = R.string.to_detail))
+                                }
+                            }
+                        }
+                        composable(
+                            route = "detail",
+                            deepLinks = listOf(
+                                navDeepLink {
+                                    uriPattern = "https://raheem.com/{id}"
+                                    action = Intent.ACTION_VIEW
+                                }
+                            ),
+                            arguments = listOf(
+                                navArgument(name = "id") {
+                                    type = NavType.IntType
+                                    defaultValue = -1
+                                }
+                            )
+                        ) { entry ->
+                            val id = entry.arguments?.getInt("id") ?: -1
+                            Box(
+                                contentAlignment = Alignment.Center,
+                                modifier = Modifier.fillMaxSize()
+                            ) {
+                                Text(text = stringResource(id = R.string.your_id, id))
+                            }
+                        }
+                    }
                 }
             }
         }
